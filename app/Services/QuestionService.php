91eaa379ae0b6bfe01b034c3f8d9e54896fafade
BeingApple <whdydql123@icloud.com>
2024-03-18 13:28:35 +0900

    답변이 채택된 질문엔 더 이상 답변을 달 수 없도록 변경했습니다.

--- app/Services/QuestionService.php
+++ app/Services/QuestionService.php
@@ -41,6 +41,11 @@ class QuestionService implements QuestionServiceInterface {
             throw new BadRequestException("작성자 본인은 답변할 수 없습니다.");
         }
 
+        // 채택된 질문이 있는 지 확인합니다.
+        if ($question->isChooseAnswer()) {
+            throw new BadRequestException("답변을 채택한 질문엔 더 이상 답변할 수 없습니다.");
+        }
+
         // 질문에 답변 갯수를 확인합니다.
         if ($question->isFulllAnswer()) {
             throw new BadRequestException("답변이 3개 이상 등록된 질문엔 더 이상 답변할 수 없습니다.");
